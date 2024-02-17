8bf56a17f039cce0387fabb532d21fd13f45e304
keyzox <keyzox@proton.me>
2024-02-17 20:38:07 +0100

    working nix-config

--- configuration.nix
+++ configuration.nix
@@ -1,136 +1,150 @@
-{ config, pkgs, ... }
+{ config, pkgs, ... }:
 
 {
-	nixpkgs.config.allowUnfree = true;
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
 
-	imports =
-	[
-		inputs.home-manager.nixosModules.home-manager
+  environment = {
+    sessionVariables = {
+    };
+  };
 
-		./hardware-configuration.nix
-	];
+  # Bootloader.
+  # boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
+  boot.blacklistedKernelModules = [ "nouveau" ];
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
 
-	sessionVariables = {
-		# USER = "adjoly";
-		# MAIL = "adjoly@student.42angouleme.fr";
-		EDITOR = "vim";
-    	VISUAL = "codium";
-		KITTY_ENABLE_WAYLAND = "1";
-	};
+  networking.hostName = "PC-ADAMNIX"; # Define your hostname.
 
-	boot.loader.efi.canTouchEfiVariables = true;
-	boot.loader.efi.efiSysMountPoint = "/boot";
-	boot.blacklistedKernelModules = [ "nouveau" ]; # nouveau hate club
-	boot.loader.grub.device = "nodev";
-	boot.loader.grub.enable = true;
-	boot.loader.grub.efiSupport = true;
-	boot.loader.grub.useOSProber = true;
+  networking.networkmanager.enable = true;
+  networking.nameservers = [ "8.8.8.8 1.1.1.1 192.168.1.1" ];
 
-	networking.hostName = "LAPTOPNIXOS-ADAM";
-	networking.networkmanager.enable = true;
-	networking.firewall.enable = false;
-	networking.nameservers = [ "8.8.8.8" "1.1.1.1" ];
+  time.timeZone = "Europe/Paris";
 
-	time.timeZone = "Europe/Paris";
+  i18n.defaultLocale = "fr_FR.UTF-8";
 
-	i18n.defaultLocale = "fr_FR.UTF-8";
-	i18n.extraLocaleSettings = {
-		LC_ADRESS = "fr_FR.UTF-8";
-		LC_IDENTIFICATION = "fr_FR.UTF-8";
-		LC_MEASUREMENT = "fr_FR.UTF-8";
-		LC_MONETARY = "fr_FR.UTF-8";
-		LC_NAME = "fr_FR.UTF-8";
-		LC_NUMERIC = "fr_FR.UTF-8";
-		LC_PAPER = "fr_FR.UTF-8";
-		LC_TELEPHONE = "fr_FR.UTF-8";
-		LC_TIME = "fr_FR.UTF-8";
-	};
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
 
-	hardware.opengl.enable = true;
-	hardware.opengl.driSupport32Bit = true;
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
+  services.xserver.displayManager.defaultSession = "gnome";
 
-	programs.hyprland = {
-		enable = true;
-		xwayland.enable = true;
-		nvidiaPatches = true;
-	};
+  hardware.opengl.enable = true;
+  hardware.opengl.driSupport = true;
+  hardware.opengl.driSupport32Bit = true;
 
-	programs.zsh.enable = true;
+  services.xserver = {
+    layout = "us";
+    videoDrivers = [ "nvidia" ];
+    xkbVariant = "intl";
+    libinput = {
+      enable = true;
+      mouse.accelProfile = "flat";
+    };
+  };
 
-	services = {
-		pipewire = {
-			enable = true;
-			alsa.enable = true;
-			alsa.support32Bit = true;
-			pulse.enable = true;
-			wireplumber.enable = true;
-    	};
-    	xserver = {
-			layout = "us";
-			xkbVariant = "altgr-intl";
-			libinput = {
-				enable = true;
-				mouse.accelProfile = "flat";
-				touchpad.accelProfile = "flat";
-    		};
+  programs.zsh.enable = true;
 
-		};
-		fstrim.enable = true;
+  console.keyMap = "us-acentos";
 
-	};
-	security.rtkit.enable = true;
-	services.xserver.libinput.enable = true;
+  services.printing.enable = true;
 
-	users.users.yosyo = {
-		shell = pkgs.zsh;
-		isNormalUser = true;
-		extraGroups = [ "networkmanager" "wheel" "audio" "input" "video" "seat" "sudo" ]; # Enable ‘sudo’ for the user.
-		# packages = with pkgs; [
-		# ];
-  	};
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
-	programs.thunar.enable = true;
-	services.gvfs.enable = true; # Mount, trash, and other functionalities
-	services.tumbler.enable = true; # Thumbnail support for images
+  services.xserver.libinput.enable = true;
 
-	environment.systemPackages = with pkgs; [
+  users.users.adjoly = {
+    shell = pkgs.zsh;
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" "i2c" "kvm" "libvirt" "input" ];
+  };
 
-    	# essentials / utils / libs / others / shitposting
-		vlc vim wget curl neofetch wayland wayland-protocols libva-utils vulkan-tools vulkan-headers vulkan-validation-layers libcamera xdg-utils mate.mate-polkit killall brightnessctl gamescope git dunst pulseaudio btop flameshot
-    	# apps
-    	discord brave spotify qbittorrent vscodium kitty thunar
-    	# rice
-    	swww macchina zsh wofi inputs.hyprsome.packages."${pkgs.system}".default inputs.gross.packages."${pkgs.system}".default gammastep
+  users.extraGroups.vboxusers.members = [ "adjoly" ];
 
-	];
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "adjoly";
 
-	fonts = {
-		fonts = with pkgs; [ jetbrains-mono google-fonts ];
-	};
-	xdg = {
-    	sounds.enable = true;
-    	portal = {
-			enable = true;
-    		xdgOpenUsePortal = true;
-			extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-		};
-	};
-	home-manager = {
-		useGlobalPkgs = true;
-		useUserPackages = true;
-		extraSpecialArgs = { inherit inputs; };
-		users.adjoly = import ./hm/home.nix;
-	};
-	nix = {
-		settings = {
-			auto-optimise-store = true;
-			experimental-features = [ "nix-command" "flakes" ];
-		};
-		gc = {
-			automatic = true;
-			dates = "weekly";
-			options = "--delete-older-than 7d";
-		};
-	};
-	system.stateVersion = "23.05";
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
+
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+#utils / utils / other
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    neofetch
+    curl
+    ddcutil
+    solaar
+#communication
+    beeper
+    discord
+    slack
+    signal-desktop
+#internet
+    brave
+    nextcloud-client
+    firefox
+    spotify
+    authy
+    plex-desktop
+#dev
+    git
+    github-cli
+    vscodium
+    gcc
+    readline
+    clang_17
+    SDL2
+    gnumake
+    norminette
+#tool for de
+    gnome.gnome-terminal
+    gnome.gnome-tweaks
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.brightness-control-using-ddcutil
+  ];
+
+  # List services that you want to enable:
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
 }
